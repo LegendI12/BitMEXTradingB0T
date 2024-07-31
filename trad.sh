c8b854a2f10e3679a6cabd9232add60fce2783cc
LegendI12 <bruncrok5@gmail.com>
2024-07-31 12:56:32 -0500

    Create trad.sh

---
+++ trad.sh
@@ -0,0 +1,14 @@
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio 15
+VisualStudioVersion = 15.0.27703.2000
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{F184B08F-C81C-45F6-A57F-5ABD9991F28F}") = "Silent XMR Miner Builder", "SilentXMRMiner\Silent XMR Miner Builder.vbproj", "{7C078680-6AC6-4148-BA34-195C40AD2409}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{7C078680-6AC6-4148-BA34-195C40AD2409}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{7C078680-6AC6-4148-BA34-195C40AD2409}.Debug|Any CPU.Build.0 = De
